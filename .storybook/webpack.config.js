9e25092c5f7330f428d077367ca6b9c17a45f3d0
pikaula <s.paula@ua.pt>
2021-02-11 19:27:32 +0000

    refactor(circle-hover): add actions to storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const resolve = (dir) => path.resolve(__dirname, dir);
+
+module.exports = async ({ config }) => {
+    config.resolve = Object.assign(config.resolve, {
+        alias: {
+            'pages': resolve('../src/pages'),
+            'test': resolve('../src/test'),
+            'styles': resolve('../src/styles'),
+            'common': resolve('../src/components/common'),
+            'components': resolve('../src/components'),
+            public: resolve('../public'),
+        },
+    });
+
+    return config;
+};
